2dc91de5c011756975b5362fb19b34ce8a38132a
zac1st1k <zacfirst@gmail.com>
2015-01-31 00:54:14 +1100

    + Winnings display

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -30,8 +30,8 @@ class ViewController: UIViewController {
     var betMaxButton:UIButton!
     var spinButton:UIButton!
     
-    let kMarginForView:CGFloat = 10.0
-    let kMarginForSlot:CGFloat = 2.0
+    let kMarginForView:CGFloat = 0.0
+    let kMarginForSlot:CGFloat = 1.0
     let kSixth:CGFloat = 1.0/6.0
     let kThird:CGFloat = 1.0/3.0
     let kHalf:CGFloat = 1.0/2.0
@@ -45,9 +45,14 @@ class ViewController: UIViewController {
     var currentBet = 0
     var winnings = 0
     
+    var timer = NSTimer()
+    var counter = 0
+    let TIME_INCREMENT = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.blackColor()
         setupContainerViews()
         setupFirstContainer(firstContainer)
         setupSecondContainer(secondContainer)
@@ -61,7 +66,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     override func viewDidAppear(animated: Bool) {
-//        showAlertWithText(header: "No More Credits", message: "Reset Game")
+        //        showAlertWithText(header: "No More Credits", message: "Reset Game")
     }
     
     func setupContainerViews() {
@@ -90,16 +95,16 @@ class ViewController: UIViewController {
     func setupSecondContainer(containerView: UIView) {
         slots = Factory.createSlots()
         for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber {
-            
             for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
                 var slotImageView = UIImageView()
-                slotImageView.backgroundColor = UIColor.yellowColor()
+                slotImageView.backgroundColor = UIColor.blackColor()
                 slotImageView.frame = CGRect(
                     x: containerView.bounds.origin.x + (containerView.bounds.size.width * CGFloat(containerNumber) * kThird),
                     y: containerView.bounds.origin.y + (containerView.bounds.size.height * CGFloat(slotNumber) * kThird),
                     width: containerView.bounds.width * kThird - kMarginForSlot,
                     height: containerView.bounds.height * kThird - kMarginForSlot)
                 slotImageView.image = slots[containerNumber][slotNumber].image
+                slotImageView.contentMode = UIViewContentMode.ScaleAspectFit
                 containerView.addSubview(slotImageView)
             }
         }
@@ -107,7 +112,7 @@ class ViewController: UIViewController {
     func setupThirdContainer(contanerView:UIView) {
         creditsLabel = UILabel()
         creditsLabel.text = "000000"
-        creditsLabel.textColor = UIColor.redColor()
+//        creditsLabel.textColor = UIColor.redColor()
         creditsLabel.font = UIFont.systemFontOfSize(16)
         creditsLabel.sizeToFit()
         creditsLabel.center = CGPoint(x: contanerView.frame.width * kSixth, y: contanerView.frame.height * kThird)
@@ -116,7 +121,7 @@ class ViewController: UIViewController {
         
         betLabel = UILabel()
         betLabel.text = "0000"
-        betLabel.textColor = UIColor.redColor()
+//        betLabel.textColor = UIColor.redColor()
         betLabel.font = UIFont.systemFontOfSize(16)
         betLabel.sizeToFit()
         betLabel.center = CGPoint(x: contanerView.frame.width * kSixth * 3, y: contanerView.frame.height * kThird)
@@ -125,7 +130,7 @@ class ViewController: UIViewController {
         
         winnerPaidLabel = UILabel()
         winnerPaidLabel.text = "000000"
-        winnerPaidLabel.textColor = UIColor.redColor()
+//        winnerPaidLabel.textColor = UIColor.redColor()
         winnerPaidLabel.font = UIFont.systemFontOfSize(16)
         winnerPaidLabel.sizeToFit()
         winnerPaidLabel.center = CGPoint(x: contanerView.frame.width * kSixth * 5, y: contanerView.frame.height * kThird)
@@ -134,7 +139,7 @@ class ViewController: UIViewController {
         
         creditsTitleLabel = UILabel()
         creditsTitleLabel.text = "Credits"
-        creditsTitleLabel.textColor = UIColor.redColor()
+//        creditsTitleLabel.textColor = UIColor.redColor()
         creditsTitleLabel.font = UIFont.systemFontOfSize(16)
         creditsTitleLabel.sizeToFit()
         creditsTitleLabel.center = CGPoint(x: contanerView.frame.width * kSixth, y: contanerView.frame.height * kThird * 2)
@@ -143,7 +148,7 @@ class ViewController: UIViewController {
         
         betTitleLabel = UILabel()
         betTitleLabel.text = "Bet"
-        betTitleLabel.textColor = UIColor.redColor()
+//        betTitleLabel.textColor = UIColor.redColor()
         betTitleLabel.font = UIFont.systemFontOfSize(16)
         betTitleLabel.sizeToFit()
         betTitleLabel.center = CGPoint(x: contanerView.frame.width * kSixth * 3, y: contanerView.frame.height * kThird * 2)
@@ -151,9 +156,9 @@ class ViewController: UIViewController {
         contanerView.addSubview(betTitleLabel)
         
         winnerPaidTitleLabel = UILabel()
-        winnerPaidTitleLabel.textColor = UIColor.redColor()
+//        winnerPaidTitleLabel.textColor = UIColor.redColor()
         winnerPaidTitleLabel.text = "Winner Paid"
-        winnerPaidTitleLabel.textColor = UIColor.redColor()
+//        winnerPaidTitleLabel.textColor = UIColor.redColor()
         winnerPaidTitleLabel.font = UIFont.systemFontOfSize(16)
         winnerPaidTitleLabel.sizeToFit()
         winnerPaidTitleLabel.center = CGPoint(x: contanerView.frame.width * kSixth * 5, y: contanerView.frame.height * kThird * 2)
@@ -161,6 +166,7 @@ class ViewController: UIViewController {
         contanerView.addSubview(winnerPaidTitleLabel)
     }
     func setupFourthContainer (containerView: UIView) {
+        containerView.backgroundColor = UIColor.whiteColor()
         resetButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
         resetButton.setTitle("Reset", forState: UIControlState.Normal)
         resetButton.sizeToFit()
@@ -174,25 +180,25 @@ class ViewController: UIViewController {
         betOneButton.center = CGPoint(x: containerView.frame.width * kEighth * 3, y:containerView.frame.height * kHalf)
         betOneButton.addTarget(self, action: "betOneButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         containerView.addSubview(betOneButton)
-
+        
         betMaxButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
         betMaxButton.setTitle("Bet Max", forState: UIControlState.Normal)
         betMaxButton.sizeToFit()
         betMaxButton.center = CGPoint(x: containerView.frame.width * kEighth * 5, y:containerView.frame.height * kHalf)
         betMaxButton.addTarget(self, action: "betMaxButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         containerView.addSubview(betMaxButton)
-
+        
         spinButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
         spinButton.setTitle("Spin", forState: UIControlState.Normal)
         spinButton.sizeToFit()
         spinButton.center = CGPoint(x: containerView.frame.width * kEighth * 7, y:containerView.frame.height * kHalf)
         spinButton.addTarget(self, action: "spinButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         containerView.addSubview(spinButton)
-
+        
     }
     
     func resetButtonPressed(sender:UIButton) {
-         hardReset()
+        hardReset()
     }
     func betOneButtonPressed(sender:UIButton) {
         println("betOneButtonPressed")
@@ -229,19 +235,16 @@ class ViewController: UIViewController {
                 updateMainView()
             }
         }
-
+        
     }
     func spinButtonPressed(sender:UIButton) {
-        println("spinButtonPressed")
-        removeSlotImageViews()
-        slots = Factory.createSlots()
-        setupSecondContainer(secondContainer)
-        
-        var winningMultiplier = SlotBrain.computeWinnings(slots)
-        winnings = winningMultiplier * currentBet
-        credits += winnings - currentBet
-//        currentBet = 0
-        updateMainView()
+        if currentBet == 0 {
+            showAlertWithText(header: "Warning", message: "Please bet first!")
+        }
+        else {
+            println("spinButtonPressed")
+            timer = NSTimer.scheduledTimerWithTimeInterval(TIME_INCREMENT, target:self, selector: Selector("spin"), userInfo: nil, repeats: true)
+        }
         
     }
     func removeSlotImageViews () {
@@ -272,6 +275,28 @@ class ViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
         presentViewController(alert, animated: true, completion: nil)
     }
-    
+    func spin () {
+        removeSlotImageViews()
+        slots = Factory.createSlots()
+        setupSecondContainer(secondContainer)
+        titleLabel.text = "Slots"
+        firstContainer.backgroundColor = UIColor.redColor()
+        self.counter++
+        println(self.counter)
+        if counter > 10 {
+            timer.invalidate()
+            counter = 0
+            println("stop")
+            var winningMultiplier = SlotBrain.computeWinnings(slots)
+            winnings = winningMultiplier * currentBet
+            credits += winnings - currentBet
+            if winnings > 0 {
+                firstContainer.backgroundColor = UIColor.greenColor()
+                titleLabel.text = "WIN!!!"
+                titleLabel.sizeToFit()
+            }
+            updateMainView()
+        }
+    }
 }
 
